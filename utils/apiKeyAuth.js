bc898c2a01e4a55b4de5cb5b6161ee93e3311024
akshayaca <akshayaca@outlook.com>
2024-07-23 19:11:39 -0400

    Env variables fixing1

--- utils/apiKeyAuth.js
+++ utils/apiKeyAuth.js
@@ -1,6 +1,6 @@
 function apiKeyAuth(req,res){
     const apiKey = req.header('api-key');
-    const validKey = process.env.API_AUTH_KEY;
+    const validKey = process.env.API_AUTH_KEY || 'abcd1234xyz';
 
     if(!apiKey || apiKey !== validKey){
         return res.status(401).json({message: 'Unauthorized'});
