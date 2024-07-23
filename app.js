e46b51116312038182b2ccb56855de8d7f9d3e51
akshayaca <akshayaca@outlook.com>
2024-07-23 18:30:16 -0400

    app.js configuration for authkey

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const employeeRoutes = require('./routes/employeeRoutes');
-
+const apiKeyAuth = require('./utils/apiKeyAuth');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(apiKeyAuth);
 app.use('/api', employeeRoutes);
 
 app.listen(port, () => {
