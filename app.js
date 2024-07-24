3b28707b0fa3360df0492f7fea58ff5d2d85b7f3
akshayaca <akshayaca@outlook.com>
2024-07-23 23:36:06 -0400

    rm mw

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const employeeRoutes = require('./routes/employeeRoutes');
-const apiKeyAuth = require('./utils/apiKeyAuth');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api', apiKeyAuth,employeeRoutes);
-app.use(apiKeyAuth);
+app.use('/api',employeeRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
