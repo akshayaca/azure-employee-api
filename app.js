511373ac54ce31ed57843aefed56e49e6398a002
akshayaca <akshayaca@outlook.com>
2024-07-23 23:29:24 -0400

    middleware2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api', employeeRoutes);
+app.use('/api', apiKeyAuth,employeeRoutes);
 app.use(apiKeyAuth);
 
 app.listen(port, () => {
