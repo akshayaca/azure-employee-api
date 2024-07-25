4b597a784292c9f9538be7f18a7413a37ea38e1e
akshayaca <akshayaca@outlook.com>
2024-07-25 15:48:30 -0400

    4

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const EmployeeController = require('../controllers/EmployeeController');
+const EmployeeController = require('../controllers/employeeController');
 const empCont = new EmployeeController();
 
 router.get('/employees', empCont.getAllEmployees);
